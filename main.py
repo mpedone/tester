c4ec271cd39f93695e33d8d2869643f329defe2f
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2026-03-06 09:31:53 -0500

    add run_count() to separate from greet()

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def main():
     print("Hello from tester!")
-    x = input("What is your name? ")
-    print(greet(x))
+    username = input("What is your name? ")
+    greet(username)
 
 def greet(n):
-    if type(n) != "str":
-        str(n)
     print(f"Hello, {n.capitalize()}. It is nice to see you.")
-    for i in range(20):
-        print(f"This has run {i+1} times, {n.capitalize()}.")
+    c = input("How many times should I run? ")
+    print(run_count(c, n.capitalize()))
+
+def run_count(x, name):
+    if not x.isnumeric():
+        return f"I'm sorry, {name}. I can't do that."
+    for i in range(int(x)):
+        print(f"This has run {i+1} times, {name}.")
     return "...And we're done."
 
 if __name__ == "__main__":
