608870b3b4e27ff36c5a67f96bce8ae0feebe634
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2026-03-06 09:21:38 -0500

    add a counter to greet()

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
 def greet(n):
     if type(n) != "str":
         str(n)
-    return f"Hello, {n.capitalize()}. It is nice to see you."
+    print(f"Hello, {n.capitalize()}. It is nice to see you.")
+    for i in range(20):
+        print(f"This has run {i+1} times, {n.capitalize()}.")
+    return "...And we're done."
 
 if __name__ == "__main__":
     main()
