32268c06acf4e4944d1556d37f8b3b974052deb7
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2026-03-06 09:54:24 -0500

    delete one text line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def main():
     print("Hello from tester!")
-    print("I'm going to greet you personally now.")
     print("I'm not sure why, but that is what the creator asks of me.")
     username = input("What is your name? ")
     print("Thank you for that, here is your personalized greeting:")
